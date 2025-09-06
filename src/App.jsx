75b9c0ea8083be5f7ccce37dca14c5c02a45acbb
Vadim <vadim.shaimanov@gmail.com>
2025-09-06 16:55:54 +0300

    Fix routes for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@
 
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import About from "./pages/Product";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />   
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<Product />} />
     </Routes>
   );
 }
